041e996a99f68ec770df20e26c5b8b18d5a66009
Martijn Boers <martijn@plebian.nl>
2020-12-09 16:08:13 +0100

    Revert "Remove mouse keys"
    This reverts commit 1a62aaa9b6785976ef12a61480c6503f0d18fdf0.

--- keyboards/preonic/rev3/rules.mk
+++ keyboards/preonic/rev3/rules.mk
@@ -5,17 +5,17 @@ MCU = STM32F303
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-BOOTMAGIC_ENABLE = yes     # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
 ## (Note that for BOOTMAGIC on Teensy LC you have to use a custom .ld script.)
-MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = yes      # Audio control and System control
-CONSOLE_ENABLE = yes       # Console for debug
+MOUSEKEY_ENABLE = yes      # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes         # Console for debug
 COMMAND_ENABLE = yes       # Commands for debug and configuration
-NKRO_ENABLE = yes          # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
-MIDI_ENABLE = no           # MIDI controls
-BLUETOOTH_ENABLE = no      # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+MIDI_ENABLE = no            # MIDI controls
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 WS2812_DRIVER = bitbang
 API_SYSEX_ENABLE = no
 
@@ -29,6 +29,6 @@ DIP_SWITCH_ENABLE = no
 
 DYNAMIC_MACRO_ENABLE = yes
 WPM_ENABLE = no
-TAP_DANCE_ENABLE = yes
+TAP_DANCE_ENABLE=yes
 
 LAYOUTS = ortho_5x12
